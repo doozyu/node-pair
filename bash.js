99ce7bf15f62687b1cf105c94193458fe59c2d03
Renee Du <doozyu@gmail.com>
2022-03-04 14:52:24 -0500

    sed commit

--- bash.js
+++ bash.js
@@ -1,7 +1,7 @@
+const pwd = require('./pwd');
 process.stdout.write('prompt >');
 process.stdin.on("data",(data)=>{
   const cmd = data.toString().trim();
-  console.log(process.cwd());
-  process.stdout.write('\nprompt >')
 }
 )
+pwd();
---
+++ pwd.js
@@ -0,0 +1,5 @@
+
+module.exports = function () {
+  console.log(process.cwd());
+process.stdout.write('\nprompt >')
+};
